5a8aa70a7c0f17de79a5c8899e8539015d250db2
Artem Petrov <artem343p@gmail.com>
2020-02-23 21:35:08 +0300

    added first migration

--- movie_rec/models.py
+++ movie_rec/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    genres = models.ManyToManyField('Genre', related_name='movies')
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(5)]
+    )
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} rated \
+            {self.movie.title} as {self.rating}"
