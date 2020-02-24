04563467acdb20587a49e92725cabc1427452ede
Artem Petrov <artem343p@gmail.com>
2020-02-24 14:24:38 +0300

    added login / logout functionality

--- movies/settings.py
+++ movies/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'movie_rec-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def register(request):
     if request.method == 'POST':
@@ -8,10 +10,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('movie_rec-home')
+            messages.success(
+                request, f'Hello {username}, your account has been created! You can now log in.')
+            return redirect('login')
         else:
             pass
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
